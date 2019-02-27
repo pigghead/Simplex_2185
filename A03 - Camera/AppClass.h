46772b90387e4a7189deb2b3fc74b602ed04708b
pigghead <justin.strongarms10@gmail.com>
2019-02-27 00:12:14 -0500

    E08 Deliverable

--- A03 - Camera/AppClass.h
+++ A03 - Camera/AppClass.h
@@ -18,7 +18,10 @@ class Application
 {
 	MyCamera* m_pCamera = nullptr; //Camera class
 	MyMeshManager* m_pMyMeshMngr = nullptr; //My Mesh Manager
-	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu"; //Programmer
+	String m_sProgrammer = "Justin Armstrong - jxa1762@rit.edu"; //Programmer
+	/******************************/
+	float m_fFov = 45.0f;	// member float variable denoting field of view
+	vector3 m_v3Eye;		// member vector3 variable denoting what we're looking at
 private:
 	static ImGuiObject gui; //GUI object
 
--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -3,7 +3,7 @@ using namespace Simplex;
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Justin Armstrong - jxa1762@rit.edu";
 
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -51,24 +51,33 @@ void Application::Display(void)
 	default:
 	case 1:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * 10.0f, ZERO_V3, AXIS_Y);
 		break;
 	case 2:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_X * 25.0f, ZERO_V3, -AXIS_Z);
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(-AXIS_Z * 15.0f, ZERO_V3, AXIS_Y);
 		break;
 	case 5:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(-AXIS_Z * 15.0f, ZERO_V3, AXIS_Y);
+		m_pCamera->SetNearFar(vector2(6.0f, 1000.0f));
 		break;
 	case 6:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(-AXIS_Z * 15.0f, ZERO_V3, AXIS_Y);
+		m_pCamera->SetNearFar(vector2(4.0f, 10.0f));
 		break;
 	case 7:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * 10.0f, ZERO_V3, -AXIS_Y);
 		break;
 	}
 
